b2e7430252b84f4efe7339cd8912324e05ff377e
lilimelgar <lilimelgar@users.noreply.github.com>
2024-03-27 16:37:53 +0100

    Update lippe_csv_to_lod_geo.py

--- src/lippe_csv_to_lod_geo.py
+++ src/lippe_csv_to_lod_geo.py
@@ -1,6 +1,6 @@
 # this script is made based on the instructions given in this
-# website: https://stackoverflow.com/questions/43524943/creating-rdf-file-using-csv-file-as-input/61445967#61445967
-# this script converts a csv file to RDF
+# tutorial: https://stackoverflow.com/questions/43524943/creating-rdf-file-using-csv-file-as-input/61445967#61445967
+# this script converts a csv file to RDF using rdflib library
 
 import pandas as pd #for handling csv and csv contents
 from rdflib import Graph, Literal, RDF, URIRef, Namespace #basic RDF handling
